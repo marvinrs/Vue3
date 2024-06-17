b16da57dddc28d3abbe0795f5a48c76caf6df906
marvinrs <72471673+marvinrs@users.noreply.github.com>
2024-06-17 16:20:15 -0300

    V-bind, v-if, uso em carrinho de compra.

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 ﻿const app = {
     data (){
         return {
+            title: 'Curso de Vue 3',
             name: 'Carlos',
-            lastName: 'Ferreira',
+            lastName: 'Ferreira',            
+            products: [
+                {
+                    title: "Imagem 1",
+                    description: "Imagem legal",
+                    image: "./assets/menino.jpg",
+                    stars: "5",
+                },
+                {
+                    title: "Imagem 2",
+                    description: "Imagem legal (2)",
+                    image: "./assets/mae_filha.jpg",
+                    stars: "0",
+                },
+            ],
+            cart: [],
+        }
+    },
+    methods: {
+        
+        addCart (product) {
+            this.cart.push(product)
+        },
+
+        inCart (product) {
+            return this.cart.indexOf(product) != -1
+        },
+
+        removeCart (product) {
+            this.cart = this.cart.filter((prod,index) => {
+                return prod != product;
+            })
+        }
+    },
+    computed: {
+        fullName () {
+            return this.name + ' ' + this.lastName
         }
     }
 }
